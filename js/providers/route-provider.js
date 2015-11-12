32e1983e22d08179ae566315028d5610494684d0
ashleyluu <ashleyluu@Ashleys-MacBook-Pro.local>
2015-11-11 17:39:18 -0800

    list styles

--- js/providers/route-provider.js
+++ js/providers/route-provider.js
@@ -6,11 +6,11 @@ var RouteProvider = function ( $stateProvider, $locationProvider ){
       templateUrl: './views/default.html',
       controller  : function($scope, $window, $log, $timeout, $map){
 
-        $scope.title = 'Final Project';
+        $scope.title = 'rawr why cant i think of a name';
         $scope.places = [
-          {image:'', name:'Tacomiendo', address:'4502 Inglewood Blvd, Culver City, CA 90230', description:'Cheap delicious Mexican food'},
-          {image:'', name:'Mohawk Bend', address:'2141 Sunset Blvd, Los Angeles, CA 90026', description:'Craft beer'},
-          {image:'', name:'Espresso Profeta', address:'1129 Glendon Ave, Los Angeles, CA 90024', description:'Coffee & pastries'},
+          {image:'', name:'Tacomiendo', address:'4502 Inglewood Blvd, Culver City', description:'Cheap delicious Mexican food'},
+          {image:'', name:'Mohawk Bend', address:'2141 Sunset Blvd, Los Angeles', description:'Craft beer'},
+          {image:'', name:'Espresso Profeta', address:'1129 Glendon Ave, Los Angeles', description:'Coffee & pastries'},
         ];
         $timeout(function(){
             $map.initialize().then(function(res){
